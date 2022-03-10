bbf88fd186cc84d5bd9ae20932e64555b9f6ef11
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-03-10 20:40:38 -0300

    Router + NavBar

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/navBar/navBar";
+import AboutMe from "./pages/aboutMe/aboutMe";
+import Gallery from "./pages/gallery/gallery";
+import Home from "./pages/home/home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="about-me" element={<AboutMe />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import OwnerSign from "./assets/Esteban-Argerich-black-high-res.png";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = styled.nav`
+  margin: 15px 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 100px;
+`;
+
+const Wrapper = styled.ul`
+  list-style-type: none;
+  display: flex;
+`;
+
+const StyledLink = styled.li`
+  font-weight: 700;
+  font-size: 1.2rem;
+  margin: 0 50px;
+
+  &::after {
+    content: "";
+    width: 0%;
+    height: 1px;
+    background: #363636;
+    display: block;
+    transition: 200ms;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+export default function NavBar() {
+  return (
+    <Nav>
+      <Logo src={OwnerSign} alt="Esteban Argerich Sign as Header Icon" />
+      <Wrapper>
+        <StyledLink>
+          <Link to="/"> Home </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link to="/gallery"> Gallery </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link to="/about-me"> About Me </Link>
+        </StyledLink>
+      </Wrapper>
+    </Nav>
+  );
+}
