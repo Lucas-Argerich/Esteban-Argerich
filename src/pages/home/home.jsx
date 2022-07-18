9f27ebc766472b632be57229e59804d8eca90e1f
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-07-18 00:31:57 -0300

    home background

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import HorizontalBackground from "./assets/backgroundOne.jpg";
+import VerticalBackground from "./assets/backgroundTwo.jpg";
+
+const Main = styled.main`
+  background: url(${VerticalBackground}) no-repeat;
+  background-size: cover;
+  height: calc(100vh - 130px);
+
+  @media (min-width: 769px) {
+    background: url(${HorizontalBackground}) no-repeat;
+    background-size: calc(80vw);
+    background-position: left top;
+  } ;
+`;
 
 export default function Home() {
-  return (
-    <main>
-        <nav></nav>
-    </main>
-  )
+  return <Main />;
 }
